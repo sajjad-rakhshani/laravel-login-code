21c4a1af94e17ce54b0016a65dd237fabcafb454
sajjad-rakhshani <sajjad.rakhsjani@gmail.com>
2023-05-27 07:02:33 +0330

    notification added

--- src/Listeners/SendLoginCode.php
+++ src/Listeners/SendLoginCode.php
@@ -2,13 +2,13 @@
 
 namespace SajjadRakhshani\LaravelLoginCode\Listeners;
 
-use App\Models\LoginCode;
+use App\Notifications\LoginCode;
 use SajjadRakhshani\LaravelLoginCode\Events\LoginRequest;
 
 class SendLoginCode
 {
     public function handle(LoginRequest $loginRequest): void
     {
-        $loginRequest->user->notify(new LoginCode());
+        $loginRequest->user->notify(new LoginCode($loginRequest->code));
     }
 }
